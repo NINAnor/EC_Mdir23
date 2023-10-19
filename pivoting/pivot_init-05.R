49b790b1d9b64910f56ec6566f09833e016fc565
Bálint Czúcz <balint.czucz@nina.no>
2023-10-19 10:47:25 +0200

    v5: with simple tables, a broad overview (ovr), and the full table with all ETs and ECTs on three levels of aggregation (full). Several errors corrected.

---
+++ pivoting/pivot_init-05.R
@@ -0,0 +1,161 @@
+
+###########################################
+###
+### Helper functions & constants for
+### 
+### Expoloring the options for "pivoting" the EC variables 
+### in the "utredning tistand" project  
+### 
+### R script by Balint
+###  Sep 2023
+###
+###########################################
+
+
+###
+### constants
+###
+l.spat.res <- c(paste0("g",1:6),"p1","p2","pp","px") #levels of d.spat.res 
+l2.spat.res <- c(g1= "~1 m",
+                 g2= "~10 m",
+                 g3= "~100 m",
+                 g4= "~1 km",
+                 g5= "~10 km",
+                 g6= "~100 km",
+                 p1= "kommune",
+                 p2= "fylke/region",
+                 pp= "annet",
+                 px= "ingen (nasj)")
+l.dir <- "pnux" %>% str_split_1("")
+l.etlink <- c("cc","fo","so","lm","na")
+
+# dat1$i.cpl3 %>% c(est1$i.cpl3) %>% unique %>% sort %>% {.[-1]} %>% dput #remove the "" #(with combinations)
+# res1$cpl0 %>% names %>% unique %>% sort %>% {.[-1]} %>% dput #remove the "" #(with combinations)
+l.cpl  <- c("ai","aqi","d","dn","e","n","o","q","qo","r","s","sq","t","tq","xd","xt")
+l.cpl1 <- l.cpl %>% str_c(collapse="") %>% str_split_1("") %>% unique %>% sort #%>% dput #(without combinations)
+
+# # dat1$d.temp.res2 %>%  unique %>% sort %>%  dput #remove the "" #(with combinations)
+# l.trs  <- c(1L, 5L, 10L)
+
+# est0 %>% select(a:m) %>% names %>% dput
+# l.et.eu <- c("a", "b", "c", "e", "f", "g", "h", "j", "k", "i", "l", "m")
+l.et.nina <- c("n1", "n21", "n22", "n31", "n32", "n41", "n42", "n51", "n52",
+               "n53", "n54", "n55", "n6", "n71", "n72", "n8a", "n8b", "nC", "nM")
+lkp.et.eu <- read_csv2(show_col_types=F, 
+  "et; et2; et6;    et_label
+    a;  ur; urban;  Settlements and other artificial areas
+    b;  cr; crop;   Croplands
+    c;  gr; grass;  Grasslands
+    e;  fo; forest; Forests and woodlands
+    f;  sh; shrub;  Heathland and shrub
+    g;  sv; SVL;    Sparsely vegetated ecosystems
+    h;  iw; wet;    Inland wetlands
+    j;  rc; river;  Rivers and Canals
+    k;  lr; lake;   Lakes and reservoirs
+    i;  co; coas.t; Coastal beaches, dunes and wetlands
+    l;  tw; coas.w; Marine inlets and transitional waters
+    m;  ma; marine; Marine ecosystems")
+
+# dat1$ect.p %>% unique %>% sort %>% dput
+l.ect <- c("A1", "A2", "B1a", "B1b", "B2", "B3", "C1", "xx") #without the "X"
+
+
+
+###
+### helpers
+###
+h.lkp <- function(x, tab) { #simple lookup of values of vector x in the second column of data.frame (or matrix) tab
+  tab[[2]][match(as.character(x), as.character(tab[[1]]))]
+  } 
+
+h.clean <- function(x) {if (mode(x)=="character") #helper fun to clean up a char vector (remove line breaks)
+  ret <- x %>% str_replace_all("[\r\n]", " ") %>% trimws else x
+  }
+
+h.rena <- function(x, na=if (mode(x)=="character") "" else 0) {  #helper fun to remove NAs w intelligent defaults
+  ret <- x %>% replace_na(na) 
+  }
+
+h.deblank <- function(x, na="") x %>% {.[!is.na(.)]} %>% {.[.!=na]} #helper to remove blank values from a char vector
+# c(1:3,NA,"",4:6) %>% h.deblank
+
+h.extract <- function(x, l) { #extract the first occurrence of any value of l[chr] as a substring in each of x[chr]
+  x %>% str_extract(str_c(sprintf("(%s)",l), collapse="|"))  
+  }
+
+h.concat <- function(x, sep=NULL) { #concatenate multiple comma separated lists into a single one
+  x %>% str_split(if (is.null(sep)) fixed(",") else sep) %>% 
+    list_c %>% trimws %>% h.deblank %>% unique %>% sort %>% 
+    str_c(collapse=if (is.null(sep)) fixed(", ") else sep)
+  }
+c("NVE", "MDir", "MDir,NINA", "MDir, , NINA", "NINA") %>% h.concat
+
+x=c("p3?", "?", "11")
+# x: a character vector of "ET status codes" (the same [(p)S(?)] notation as in cols n1:nM in "main")
+h.et.getS <- function(x) { #get the status (S/i.st: S=1-3) out from a vector of ET codes 
+  x %>% str_extract("[:digit:]") %>% as.integer %>% replace_na(0)
+  }
+h.et.getp <- function(x) { #get the "partiality" (p: "p" or "") out from a vector of ET codes 
+  x %>% str_extract("p") %>% replace_na("")
+  }
+h.et.agac <- function(x) { #across-ET aggregation of "ET status codes", rules:
+  #    -- if none of sel are blank or "p" --> max(X)
+  #    -- if some of sel are blank or "p" --> "p"+max(X)
+  #    -- if all of sel are blank --> blank
+  if (all(x %in% c("","?"))) return("")
+  ret <- x %>% h.et.getS %>% max %>% as.character
+  if (any(x %in% c("","?") | h.et.getp(x)=="p")) str_c("p",ret) else ret
+  }
+
+# x=LETTERS[1:5]; et1=c("p3?","2?","11", "2","?"); pt="C2"; fun=c("A","B","d")
+# x=1:5; et1=c("p3?","2?","11", "2","?"); pt="C2"; fun=~sum(c(.x,1)); .na=NULL
+# x=LETTERS[1:5]; et1=c("p1?","1?","11", "","?"); pt="C2"; fun=c("A","B","d")
+
+h.summarise <- function(x, st1, fun=NULL, pt="P1", plus=NULL, .na=NA) {
+  # x:    vec[n]: values to summarise (=aggregate)
+  # st1:  char[n]: a vector of "ET status codes" (the [(p)S(?)] notation)
+  # pt:   char[1]: a 2+1 byte "projection type" code:  
+  #         1: C/P: consider just the vars w complete coverage (C), or partial vars, too (P)
+  #         2: 1/2/3: consider only vars w status equal to or higher than this
+  # fun:  aggregation function:
+  #         NULL: n()
+  #         any function/lambda (with a single vector argument returning a (named) vector of constant type and length) 
+  #         a char vector: each of its elements gets counted in x
+  # na:   the value returned for an empty selection (if fun is a function)  
+  # plus: logi[n]: an optional additional filter (e.g. one based on i.cpl)
+  sel <- h.et.getS(st1)>=h.et.getS(pt)
+  sel <- sel & !(is.na(x))
+  if (str_sub(pt,1,1)=="C") sel <- sel & (h.et.getp(st1)=="")
+  if (!is.null(plus)) sel <- sel & plus
+  if (is.null(fun)) return(sum(sel))
+  if (is.character(fun)) {
+    map(fun, ~sum(as.character(x[sel])==.x)) %>% set_names(fun) %>% as_tibble
+   }else{
+     # cat(paste(x,st1,sel,sep=":"),"\n")
+     if (any(sel)) exec(rlang::as_function(fun), x[sel]) else .na
+    }
+  }
+
+# x=LETTERS[1:5]; w=c(0,2,1,0,1); fun=c("A","B","d")
+h.summarise2 <- function(x, w, fun=NULL, .na=NA) {
+  # x:    vec[n]: values to summarise (=aggregate)
+  # w:    int[n]: a vector of integer weights (indicator "counts") for each element in x  
+  # fun:  aggregation function:
+  #         NULL: sum(w)
+  #         any function/lambda (with a single vector argument returning a (named) vector of constant type and length) 
+  #         a char vector: each of its elements gets counted in x
+  # na:   the value returned for an empty selection (if fun is a function)  
+  # cat(paste(x,w,sep=":",collapse=" | "),"\n")
+  if (is.null(fun)) return(sum(w))
+  if (length(w)==0|sum(w)==0) return(.na)
+  x <- rep(x, times=w)
+  if (is.character(fun)) {
+    map(fun, ~sum(as.character(x)==.x)) %>% set_names(fun) %>% as_tibble
+  }else{
+    # cat(paste(x,st1,sel,sep=":"),"\n")
+    if (length(x)>=1L) exec(rlang::as_function(fun), x) else .na
+    }
+  }
+
+
+
---
+++ pivoting/pivot_test-05.R
@@ -0,0 +1,295 @@
+###########################################
+###
+### Expoloring the options for "pivoting" the EC variables 
+### in the "utredning tistand" project  
+### 
+### R script by Balint
+###  Sep 2023
+###
+###########################################
+
+library(tidyverse)
+library(googledrive)
+library(googlesheets4)
+
+vv="05" #version number
+source(str_c("pivoting/pivot_init-",vv,".R")) #initialise constants & helper functions
+# setwd("C:/Users/balint.czucz/OneDrive - NINA/_nina/fagsystem_tilstand/")
+
+# connect to the "metadatabase" spreadsheet:
+sht <-   drive_get("https://docs.google.com/spreadsheets/d/1nmg1yT1rbQBVCIoAJ8y-ID1q5RTnJlTUsKwP1UA-DVM") %>%
+  {print(sheet_names(.)); I(.)} ### tilstand_metadb
+1
+1
+
+
+###
+### Load in candidate EC vars from the metadb
+###
+
+dat0 <- range_read(sht, "main", skip=7, col_types="c", na=character()) %>%  # read in raw table  
+  select(-starts_with("..")) %>%
+  filter(i.id0!="") %>%      # remove empty lines
+  filter(h.clean(tmp1)!="1") %>% # remove "deleted" lines
+  filter(h.clean(tmp1)!="2") %>% # remove "temporarily deleted" lines (TODO: check these!)
+  rename(cls=ect.p, chr=grp) %>% #a bit more harmonised names (cls: ECT class, chr: ECT subclass/characteristic(-group))
+  {.}
+
+dat1 <- dat0 %>% # cleanup & add missing vars
+  mutate(d.startdate2= d.startdate %>% str_remove_all(fixed("?")) %>% as.integer) %>%                                       
+  mutate(d.temp.res2=  d.temp.res %>% str_remove_all(fixed("?")) %>% {if_else(.=="irr", Inf, as.integer(.))}) %>%   # Inf is the irregular / unknown repeat period                                      
+  mutate(d.spat.res2=  h.extract(d.spat.res, l.spat.res) %>% ordered(c(l.spat.res,"NA")) %>% replace_na("NA")) %>%  # "NA" is the "highest"/"worst" level for sorting
+  mutate(i.etlink2=    h.extract(i.etlink, l.etlink)) %>%                                    
+  mutate(r.dir2=       h.extract(r.dir, l.dir)) %>%                                             
+  mutate(i.cpl2=       str_remove(i.cpl, fixed("."))) %>%                                       
+  # mutate(i.cpl3=       h.extract(i.cpl, l.cpl1)) %>%   # i.cpl (simplified)
+  mutate(ect.s2= map2(ect.s1, ect.s2,    # secondary ECT types
+                     ~c(.x %>% h.deblank, .y %>% str_split_1(",") %>% trimws %>% h.deblank)) %>%
+           map_chr(~str_c(.x, collapse=","))) %>%
+  mutate(i.host2= i.host %>% 
+           str_remove_all("\\([^)]*\\)") %>%  # remove everything in parentheses
+           str_remove_all(fixed("*")) %>%
+           str_split("[,&+]") %>%
+           # map(h.clean) %>% map(~replace_na(.x,"")) %>% map(trimws) %>% map(h.deblank) %>% map(sort) %>%
+           map(h.clean) %>% map(trimws) %>% map(h.deblank) %>% map(sort) %>%
+           map(~.x[!str_detect(.x, "Various")]) %>%  # remove an annoying label (for "misc")
+           map_chr(~str_c(.x, collapse=","))) %>%
+  mutate(id.no2= i.id0 %>%     # id.0 #TODO: swap to i.replaces
+           map(~(str_split_1(.x, ","))) %>% 
+           map(trimws) %>% map(sort) %>%
+           map_chr(~str_c(.x, collapse=","))) %>%
+  {.} %>%   # TODO: ect.no
+  # mutate(novar= map(i.id0, ~(str_split_1(.x, ",") %>% trimws %>% h.deblank))) %>% 
+  pack(ETs= n1:nM) %>% mutate(ETs=ETs %>% as.list %>% list_transpose) %>%     # transforming ET status codes into a nested vector 
+  ##################################################################################### creating "status codes" for the Estat ETs: 
+  mutate(ETs= map(ETs, function(x) c(x, a=h.et.agac(x[c("n1")])))) %>%                #a, ur: Settlements and other artificial areas
+  mutate(ETs= map(ETs, function(x) c(x, b=h.et.agac(x[c("n21")])))) %>%               #b, cr: Croplands
+  mutate(ETs= map(ETs, function(x) c(x, c=h.et.agac(x[c("n22","n51","n52")])))) %>%   #c, gr: Grasslands
+  mutate(ETs= map(ETs, function(x) c(x, e=h.et.agac(x[c("n31","n32")])))) %>%         #e, fo: Forests and woodlands
+  mutate(ETs= map(ETs, function(x) c(x, f=h.et.agac(x[c("n53","n54")])))) %>%         #f, sh: Heathland and shrub
+  mutate(ETs= map(ETs, function(x) c(x, g=h.et.agac(x[c("n55","n6")])))) %>%          #g, sv: Sparsely vegetated ecosystems
+  mutate(ETs= map(ETs, function(x) c(x, h=h.et.agac(x[c("n41")])))) %>%               #h, iw: Inland wetlands
+  mutate(ETs= map(ETs, function(x) c(x, j=h.et.agac(x[c("n71")])))) %>%               #j, rc: Rivers and Canals
+  mutate(ETs= map(ETs, function(x) c(x, k=h.et.agac(x[c("n72")])))) %>%               #k, lr: Lakes and reservoirs
+  mutate(ETs= map(ETs, function(x) c(x, i=h.et.agac(x[c("nC")])))) %>%                #i, co: Coastal beaches, dunes and wetlands
+  mutate(ETs= map(ETs, function(x) c(x, l=h.et.agac(x[c("n8a","n8b","n42")])))) %>%   #l, tw: Marine inlets and transitional waters
+  mutate(ETs= map(ETs, function(x) c(x, m=h.et.agac(x[c("n8a","n8b")])))) %>%         #m, ma: Marine ecosystems
+  select(-starts_with("tmp"), -ends_with(".old")) %>%
+  mutate(all="all") %>% # aux col putting all ECTs in a single group 
+  {.}
+# "abc (xyv 12) 173+a (...!)" %>% str_remove_all("\\([^)]*\\)") 
+
+if (FALSE) { # optional testing phase...
+  dat1 %>% pluck("i.name.en",9) # Lirype
+  dat1 %>% pluck("ETs",9)
+  dat1 %>% pluck("ETs",9) %>% h.et.getS
+  dat1 %>% pluck("ETs",9) %>% h.et.getp
+  dat1 %>% pluck("ETs",9) %>% names
+  head(select(dat1,i.name.no)) #---> TODO: correct encoding!
+  dat1 %>% select(d.spat.res, i.etlink, r.dir, i.cpl) %>% map(table)
+  dat1 %>% select(d.spat.res2) %>% map(table)
+  dat1 %>% select(d.spat.res2, i.etlink2, r.dir2, i.cpl2) %>% map(table)
+  dat1 %>% select(i.host2, ect.s2) %>% map(table)
+  dat1 %>% pluck("i.name.no",3)
+  dat1 %>% select("i.name.no") %>% head %>% write_csv("tmp-balint/test.csv") #UTF8-ban vannak a szovegek, excelben nem jol nyilik :(
+  }
+
+
+###
+### quick dumps from the metadb (following the structure of Erik's demo forest table)
+###
+
+# ii="e"
+out1 <- NULL
+for (ii in lkp.et.eu$et) {
+  tmp <- dat1 %>%
+    mutate(.st = ETs %>% map_chr(~pluck(.x, ii)) %>% unname) %>% # the st of indis for the chosen ET (a temp column)
+    filter(.st!="") %>%
+    select(-ETs) %>%
+    # mutate(i.cpl2b= if_else(i.cpl2=="", "yes","no")) %>%
+    mutate(d.spat.res2b= l2.spat.res[d.spat.res2] %>% unname) %>%
+    mutate(ET.estat= str_c(ii, ": ", h.lkp(ii, select(lkp.et.eu,et,et6)))) %>%
+    select(ET.estat, i.id0, i.name.no, 
+           # Dev.status=.st, 
+           Datakilde= i.host, `Startar`= d.startdate2, 
+           `Frekvens (ar)`= d.temp.res2, `Romlig opplosning`= d.spat.res2b, 
+           ECT= cls, Egenskap= ect.no, `Samsvar m/tilstand`= r.dir, 
+           Ref.verdi= r.opt) 
+  out1 <- bind_rows(out1, tmp) 
+  }
+
+out1 %>% 
+  write_excel_csv2(str_c("tmp-balint/simple-table-", vv, format(now(), "-%m%d-%H%M.csv")), na="")
+
+
+
+###
+### Load the Estat (proto)vars (=data needs) from the metadb
+###
+est0 <- range_read(sht, "estat", skip=4, col_types="c", na=character()) %>%  # potentially requested mandatory&voluntary EC indicators (by ESTAT)
+  select(-starts_with("."), -starts_with("v5.id2")) %>%
+  filter(bc.v5id!="") %>%      # remove empty lines
+  # filter(h.clean(tmp1)!="1") %>% # remove "deleted" lines
+  rename(cls=ect.p, chr=grp, r.dir=Dir) %>% #a bit more harmonised names
+  {.}
+est1 <- est0 %>% #cleanup
+  pack(ETs= a:nM) %>% mutate(ETs=ETs %>% as.list %>% list_transpose) %>%
+  mutate(id.e.all= ETs %>% map(~(.x[lkp.et.eu$et])) %>% map(h.deblank) %>% map(trimws) %>% map(sort) %>% map_chr(~str_c(.x,collapse=","))) %>% #TOCHK: remove the "!" showing mandatoryness?  
+  mutate(r.dir2= h.extract(r.dir, l.dir)) %>%
+  mutate(i.cpl2= str_remove(i.cpl, fixed("."))) %>%                                       
+  # mutate(i.cpl3= h.extract(i.cpl, l.cpl1)) %>%   # i.cpl (simplified)
+  mutate(ect.s2= map2(ect.s1, ect.s2,    # secondary ECT types
+                      ~c(.x %>% h.deblank, .y %>% str_split_1(",") %>% trimws %>% h.deblank)) %>%
+           map_chr(~str_c(.x, collapse=","))) %>%
+  mutate(all="all") %>% # aux col putting all ECTs in a single group 
+  {.}
+# est1 %>% select(r.dir2) %>% map(table)
+
+
+###
+### flexibly aggregate the info on candidate EC vars 
+###
+
+l.chr <- dat1$chr %>% unique %>% sort 
+# gg.lkp <- tibble(gg= "all", .col= "all") %>%
+#   bind_rows(tibble(gg= l.ect, .col= "cls")) %>%
+#   bind_rows(tibble(gg= l.chr, .col= "chr")) 
+# deframe(gg.lkp)
+gg.cols <- c("all", "cls", "chr")
+et.cols <- c("all", lkp.et.eu$et)
+
+# et_="e"   # <any et name> // all
+# gg_="chr" # chr // cls // all
+res0 <- NULL
+
+for (et_ in et.cols) {  ### [13] iterate across ETs (each type of the EU typology: et.eu / "all" )
+  for (gg_ in gg.cols) {  ### [3//34] (indirectly) iterate across "EC chars" (groups of vars as defined on tab "cw": chr / cls / "all")
+
+    # .st = dat1$ETs %>% map_chr(~pluck(.x, et_)) %>% unname
+    dat1.ch <- dat1 %>% ##### dat1: Norwegian vars ||ETs contains "status codes" (st)
+      mutate(.st = if(et_=="all") i.st else ETs %>% map_chr(~pluck(.x, et_)) %>% unname) %>% # the st of indis for the chosen ET (a temp column)
+      mutate(.pl = i.cpl=="") %>% # the "plus" argument for the h.summarise function calls 
+      mutate(gg = .data[[gg_]]) %>% #TODO: "{{gg_}}" if supplied as fn argument! 
+      group_by(gg) %>%
+      summarise(nn=        h.summarise(.st, .st), 
+                nn_c1=     h.summarise(.st, .st, NULL, "C1"), # N of vars that cover the whole ET
+                nn_p2=     h.summarise(.st, .st, NULL, "P2"), # N of vars that are at or above status 2
+                nn_p1p=    h.summarise(.st, .st, NULL, "P1", .pl), # N of vars that fully comply w SEEA EA 
+                nn_c2p=    h.summarise(.st, .st, NULL, "C2", .pl), # N of vars that meet all 3 above ("readily available")
+                st_max=    h.summarise(h.et.getS(.st), .st, max),  #inf
+                st0=       h.summarise(as.character(h.et.getS(.st)), .st, as.character(1:3)), #packed cols
+                dir0.n=    h.summarise(r.dir2, .st, l.dir), #packed cols
+                tsd=       h.summarise(d.startdate2, .st, ~min(.x, na.rm=T)), 
+                tsd_c2p=   h.summarise(d.startdate2, .st, ~min(.x, na.rm=T), "C2", .pl), 
+                trs=       h.summarise(d.temp.res2, .st, ~min(.x, na.rm=T)), 
+                trs_c2p=   h.summarise(d.temp.res2, .st, ~min(.x, na.rm=T), "C2", .pl),  
+                trs0=      h.summarise(d.temp.res2 %>% as.character %>% paste0("y",.), .st, 
+                                       d.temp.res2 %>% unique %>% sort %>% as.character %>% paste0("y",.) %>% c("NA"), .na="NA"), # packed
+                # srs=       h.summarise(d.spat.res2, .st, ~min(fct_c(.x,factor("")), na.rm=T)), 
+                srs=       h.summarise(d.spat.res2, .st, ~min(.x, na.rm=T), .na=vctrs::vec_cast("NA", d.spat.res2)), 
+                srs_c2p=   h.summarise(d.spat.res2, .st, ~min(.x, na.rm=T), "C2", .pl, .na=vctrs::vec_cast("NA", d.spat.res2)),   
+                srs0=      h.summarise(d.spat.res2, .st, d.spat.res2 %>% levels), # packed
+                etl0=      h.summarise(i.etlink2, .st, l.etlink), #packed cols
+                cpl_ok.n=  h.summarise(if_else(i.cpl2=="",1,0), .st, sum), #compliant count
+                # cpl0.n=    h.summarise(i.cpl3,  .st, l.cpl1),  #packed cols
+                cpl0.n=    h.summarise(i.cpl2,  .st, ~str_c(.x, collapse=""), .na=""),  #packed cols
+                ect_s.n=   h.summarise(ect.s2,  .st, h.concat, .na=""),
+                hosts=     h.summarise(i.host2, .st, h.concat, .na=""),
+                iid.n=     h.summarise(id.no2,  .st, h.concat, .na="")) %>%
+      # separate_wider_delim(gg, " | ", names=c("cls", "echar"), cols_remove=F) %>%
+      mutate(dir_d.n= dir0.n %>% as.list %>% list_transpose %>% map2_chr(nn, ~c(names(.x)[.x/.y >= 2/3],"x")[1])) %>% 
+      mutate(et=et_, lvl=gg_) %>% relocate(et,lvl,gg) %>%
+      {.}
+    # dat1.ch$dir0.n %>% as.list %>% list_transpose %>% # pluck(9)
+    #   map2_chr(nn, ~names(.x)[.x/.y >= 1/3]) %>% {c(.,"x")[1]}
+      
+    est1.ch <- est1 %>% ##### est1: the Estat vars (info needs) || ETs contains "indicator ids" (ii)
+      mutate(.ii  = if(et_=="all") id.e.all else ETs %>% map_chr(~pluck(.x, et_)) %>% unname) %>% # indi ids (and mandatoryness) for the chosen ET (a temp column)
+      mutate(.iin = str_count(.ii, fixed("."))) %>% # number of indis (mand+vol) for each record (=family)
+      mutate(.iiM = str_count(.ii, fixed("!"))) %>% # number of mandatory indis for each record (=family)
+      # pluck(".iin") %>% sign %>% print
+      mutate(gg = .data[[gg_]]) %>% #TODO: "{{gg_}}" if supplied as fn argument! 
+      group_by(gg) %>%
+      summarise(nm=         h.summarise2(.iiM, sign(.iin), sum, 0), 
+                nv=         h.summarise2(.iin, sign(.iin), sum, 0),
+                dir0.e=     h.summarise2(r.dir2, .iin, l.dir), #packed cols
+                cpl_ok.e=   h.summarise2(if_else(i.cpl2=="",1, 0), .iin, sum, 0), #compliant count
+                cpl0.e=     h.summarise2(i.cpl2, sign(.iin), ~str_c(.x, collapse=""), .na=""),  #concat
+                ect_s.e=    h.summarise2(ect.s2, .iin, h.concat, .na=""),
+                iid.e =     h.summarise2(.ii,  sign(.iin), h.concat, .na="")) %>%
+      mutate(nv=nv-nm) %>% # to avoid double counting of the mandatory ones...
+      mutate(dir_d.e= dir0.e %>% as.list %>% list_transpose %>% map2_chr(nm+nv, ~c(names(.x)[.x/.y >= 2/3],"x")[1])) %>% 
+      mutate(et=et_, lvl=gg_) %>% relocate(et,lvl,gg) %>%
+      {.}
+    # est1.ch %>% select(cpl0.e) %>% map(table)
+    
+    tmp <- dat1.ch %>%   ##### tmp: merging the info on "available & needed"
+      full_join(est1.ch, by=c("et","lvl","gg")) %>%
+      mutate(across(where(~!(typeof(.x)=="list"|"factor" %in% class(.x))), h.rena)) %>% # eliminate NAs (I have to, because of structural NAs)
+      mutate(across(where(~typeof(.x)=="list"), ~(map(.x, h.rena) %>% as_tibble))) %>%
+      mutate(n= nn+nv+nm) %>%
+      mutate(cpl_ok =  cpl_ok.n + cpl_ok.e) %>% # (the full share will be (cpl_ok.n+cpl_ok.e)/(nn+nv+nm))
+      # mutate(dir_all = str_c(dir_all.n,dir_all.e)) %>%
+      # mutate(cpl0 = as_tibble(as.matrix(cpl0.n)+as.matrix(cpl0.e))) %>%
+      mutate(cpl0 = str_c(cpl0.n, cpl0.e)) %>%
+      mutate(cpl0 = map(l.cpl1, ~str_count(cpl0, fixed(.x))) %>% set_names(l.cpl1) %>% as_tibble) %>%
+      mutate(dir0 = as_tibble(as.matrix(dir0.n)+as.matrix(dir0.e))) %>%
+      mutate(dir_d= dir0 %>% as.list %>% list_transpose %>% map2_chr(nm+nv, ~c(names(.x)[.x/.y >= 2/3],"x")[1])) %>% 
+      rowwise %>% mutate(ect_s = h.concat(c(ect_s.n, ect_s.e))) %>%
+      mutate(iid = h.concat(c(iid.n, iid.e))) %>% ungroup %>%
+      {.}
+    
+    res0 <- bind_rows(res0, tmp)
+    # res0 <- append(res0, list(tmp))
+    }
+  }
+
+tmp <- lkp.et.eu %>% select(et, et_label) %>% deframe %>% c(all="All ecosystem types",.)
+
+res1 <- res0 %>%
+  filter(!(lvl=="chr" & n==0)) %>%  #drop the irrelevant characteristics 
+  # mutate(et_label= str_c(et, tmp[et], sep=": ")) %>%
+  mutate(lvl= ordered(lvl, levels=gg.cols)) %>%
+  mutate(et= ordered(et, levels=et.cols)) %>%
+  arrange(et, lvl, gg) %>%
+  {.}
+
+res1 %>% 
+  select(-cpl0.n,-cpl0.e) %>%
+  unpack(where(~typeof(.x)=="list"), names_sep=".") %>%
+  write_csv(str_c("tmp-balint/res_full-", vv, format(now(), "-%m%d-%H%M.csv")), na="")
+
+
+###
+### make an overview table w 2 sub-tables (rows: ECT_grps, cols: ETs)
+# #   A: status codes + them.coverage
+# #   B: M / v
+
+tmp1 <- res1 %>% 
+  filter(lvl=="chr") %>%
+  filter(gg!="" & str_sub(gg,1,2)!="X ") %>%
+  filter(et!="all") %>%
+  mutate(value=if_else(nn==0,"",str_c(st_max, if_else(nn_c1==0,"'","")))) %>%
+#   mutate(et=as.character(et)) %>%
+#   select(gg,et,value) 
+# tmp1 %>%
+#   group_by(gg, et) %>%
+#   summarise(n = n(), .groups = "drop") %>%
+#   filter(n > 1L) 
+# tmp1 %>%
+  pivot_wider(id_cols= gg, names_from= et, names_prefix= "st.", values_from= value, values_fill="") 
+tmp2 <- res1 %>% 
+  filter(lvl=="chr") %>%
+  filter(gg!="" & str_sub(gg,1,2)!="X ") %>%
+  filter(et!="all") %>%
+  mutate(value=if_else(nm>0,"M",if_else(nv>0,"v",""))) %>%
+  pivot_wider(id_cols= gg, names_from= et, names_prefix= "mv.", values_from=value, values_fill="")
+ovr1 <- full_join(tmp1,tmp2) %>%
+  arrange(gg) %>%
+  mutate(ect= gg %>% str_split_i(fixed("|"),1) %>% trimws) %>%
+  relocate(ect) %>%
+  {.}
+
+ovr1 %>%
+  write_csv(str_c("tmp-balint/res_ovr-", vv, format(now(), "-%m%d-%H%M.csv")), na="")
+
